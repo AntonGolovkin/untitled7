50dc017b22443aeb3f8d6e378837b8eb488de5c9
Антон <toni.golovkin.89@inbox.ru>
2021-06-07 14:38:48 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    TextArea mainTextArea;
+    @FXML
+    TextField mainTextField;
+
+    public void sendAction(){
+        mainTextArea.appendText(mainTextField.getText() + "\n");
+        
+    }
+}
